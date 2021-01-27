0a838bc30498759bc8799337c744f03b52177116
Shaw <cuzval@gmail.com>
2021-01-27 17:31:19 +0800

    Main accomplish

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,26 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Row from './Row'
 import './Main.css'
 
 export default class Main extends Component {
-    render() {
-      const {todos, handleToggleAccomplish, handleDelete} = this.props
+  static propType = {
+    todos: PropTypes.array.isRequired,
+    handleToggleAccomplish: PropTypes.func.isRequired,
+    handleDelete: PropTypes.func.isRequired
+  }
 
-        return (
-            <ul className="todo-main">
-              {
-                todos.map(todo => <Row key={todo.id} 
-                  {...todo}
-                  handleToggleAccomplish={handleToggleAccomplish}
-                  handleDelete={handleDelete} />)
-              }
-          </ul>
-        )
-    }
+  render() {
+    const {todos, handleToggleAccomplish, handleDelete} = this.props
+      return (
+          <ul className="todo-main">
+            {
+              todos.map(todo => <Row key={todo.id} 
+                {...todo}
+                handleToggleAccomplish={handleToggleAccomplish}
+                handleDelete={handleDelete} />)
+            }
+        </ul>
+      )
+  }
 }
