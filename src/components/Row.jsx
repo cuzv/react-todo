23af7dbf187d10ef49a5cb40cf57201caf6eb2b7
Shaw <cuzval@gmail.com>
2021-01-27 15:38:47 +0800

    Import template

---
+++ src/components/Row.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Row extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
